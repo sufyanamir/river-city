a0fa21f6108644760c9ee0a464097c9cc5487458
msalman326 <salmanrazzaq326@gmail.com>
2023-10-17 17:59:24 +0500

    check

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- <link href="https://cdn.tailwindcss.com" rel="stylesheet"> -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" />
   <link rel="stylesheet" href="{{ asset('assets/css/icons.css') }}" />
