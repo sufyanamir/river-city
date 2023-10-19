1984c5717f939dc71c699d11e00676edca0f577f
msalman326 <salmanrazzaq326@gmail.com>
2023-10-19 18:58:30 +0500

    finalSideBar

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,9 +1,7 @@
-function dropdown() {
-  document.querySelector("#submenu").classList.toggle("hidden");
-  document.querySelector("#arrow").classList.toggle("rotate-0");
+function dropdown(submenuId, arrowId) {
+  document.querySelector("#" + submenuId).classList.toggle("hidden");
+  document.querySelector("#" + arrowId).classList.toggle("rotate-180");
 }
-dropdown();
-
 function openSidebar() {
   document.querySelector(".sidebar").classList.toggle("w-[0]");
   document.querySelector(".main-container").classList.toggle("ml-[0px]");
