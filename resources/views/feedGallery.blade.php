179ed167ec5319c74e8a77d79e3a9dc6608c582e
msalman326 <salmanrazzaq326@gmail.com>
2023-10-28 22:51:17 +0500

    feedGallerylowerDiv

--- resources/views/feedGallery.blade.php
+++ resources/views/feedGallery.blade.php
@@ -1,3 +1,188 @@
 @include('layouts.header')
+<div class="my-4">
+    <h1 class=" text-2xl font-semibold mb-3">Gallery</h1>
+    <div class=" bg-white w-full rounded-lg shadow-lg">
+        <div class="">
+            <h3 class="m-5">top headings and serach box</h3>
+        </div>
+        <hr class="bg-gray-900">
+        <div class="mt-2 grid sm:grid-cols-11 bg-[#F5F5F5] rounded-[10px] m-2">
+            <div class="col-span-5 p-2 flex justify-between">
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Estimate Name</h3>
+                    <p class=" font-medium text-[17px]/[25px] text-[#858585]">
+                        Customer Name / 949-300-9632
+                        65 Water St, Newburyport, MA, 01950
+                    </p>
+                </div>
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Images</h3>
+                    <p class="font-medium text-[17px]/[25px] text-[#858585]">12</p>
+                </div>
+            </div>
+            <div class="col-span-1">
+                <hr class=" bg-black ">
+            </div>
+            <div class="col-span-5 grid grid-cols-5 mx-2 my-auto"> 
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="mt-2 grid sm:grid-cols-11 bg-[#F5F5F5] rounded-[10px] m-2">
+            <div class="col-span-5 p-2 flex justify-between">
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Estimate Name</h3>
+                    <p class=" font-medium text-[17px]/[25px] text-[#858585]">
+                        Customer Name / 949-300-9632
+                        65 Water St, Newburyport, MA, 01950
+                    </p>
+                </div>
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Images</h3>
+                    <p class="font-medium text-[17px]/[25px] text-[#858585]">4</p>
+                </div>
+            </div>
+            <div class="col-span-1">
+                
+            </div>
+            <div class="col-span-5 grid grid-cols-5 mx-2 my-auto">
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="mt-2 grid sm:grid-cols-11 bg-[#F5F5F5] rounded-[10px] m-2">
+            <div class="col-span-5 p-2 flex justify-between">
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Estimate Name</h3>
+                    <p class=" font-medium text-[17px]/[25px] text-[#858585]">
+                        Customer Name / 949-300-9632
+                        65 Water St, Newburyport, MA, 01950
+                    </p>
+                </div>
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Images</h3>
+                    <p class="font-medium text-[17px]/[25px] text-[#858585]">5</p>
+                </div>
+            </div>
+            <div class="col-span-1">
+                
+            </div>
+            <div class="col-span-5 grid grid-cols-5 mx-2 my-auto">
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="mt-2 grid sm:grid-cols-11 bg-[#F5F5F5] rounded-[10px] m-2">
+            <div class="col-span-5 p-2 flex justify-between">
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Estimate Name</h3>
+                    <p class=" font-medium text-[17px]/[25px] text-[#858585]">
+                        Customer Name / 949-300-9632
+                        65 Water St, Newburyport, MA, 01950
+                    </p>
+                </div>
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Images</h3>
+                    <p class="font-medium text-[17px]/[25px] text-[#858585]">20</p>
+                </div>
+            </div>
+            <div class="col-span-1">
+                
+            </div>
+            <div class="col-span-5 grid grid-cols-5 mx-2 my-auto">
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="mt-2 grid sm:grid-cols-11 bg-[#F5F5F5] rounded-[10px] m-2">
+            <div class="col-span-5 p-2 flex justify-between">
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Estimate Name</h3>
+                    <p class=" font-medium text-[17px]/[25px] text-[#858585]">
+                        Customer Name / 949-300-9632
+                        65 Water St, Newburyport, MA, 01950
+                    </p>
+                </div>
+                <div class="pl-3">
+                    <h3 class="text-[22px]/[25.78px] text-[#323C47] font-bold">Images</h3>
+                    <p class="font-medium text-[17px]/[25px] text-[#858585]">12</p>
+                </div>
+            </div>
+            <div class="col-span-1">
+                
+            </div>
+            <div class="col-span-5 grid grid-cols-5 mx-2 my-auto">
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image2.svg')}}" alt="">
+                </div>
+                <div class="col-span-1">
+                    <img src="{{asset('assets/images/feed-gallery-image1.svg')}}" alt="">
+                </div>
+            </div>
+        </div>
 
+    </div>
+
+</div>
 @include('layouts.footer')
