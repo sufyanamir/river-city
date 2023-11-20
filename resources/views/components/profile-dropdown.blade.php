31fbde6e61d18fa370ae287c423e6fa5b92d47ff
sufyanamir <sufi.canola@gmail.com>
2023-11-20 14:34:23 +0500

    added build to git and added links to navbar.

--- resources/views/components/profile-dropdown.blade.php
+++ resources/views/components/profile-dropdown.blade.php
@@ -11,7 +11,7 @@
     <div id="profile-menu" class=" topbar-manuLeaving absolute right-1 z-10 mt-2 w-56 origin-top-right rounded-md bg-[#930027] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
         <div class="py-1" role="none">
             <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-            <a href="#" class=" block px-4 py-2 text-sm bg-[#edf2f7] text-[#930027]" role="menuitem" tabindex="-1" id="menu-item-0"><b>Profile</b></a>
+            <span class=" block px-4 py-2 text-sm bg-[#edf2f7] text-[#930027]" role="menuitem" tabindex="-1" id="menu-item-0"><b>Profile</b></span>
             <a href="/settings" class=" sidebar-link text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-1">
                 <div class="flex justify-start">
                     <img class=" plain-icon" src="{{ asset('assets/icons/single-user.svg') }}" alt="icon">
--- resources/views/components/quick-add-btn.blade.php
+++ resources/views/components/quick-add-btn.blade.php
@@ -7,10 +7,10 @@
     <div id="topbar-menu" class=" topbar-manuLeaving absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-[#930027] shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
         <div class="py-1" role="none">
             <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-            <a href="#" class=" block px-4 py-2 text-sm bg-[#edf2f7] text-[#930027]" role="menuitem" tabindex="-1" id="menu-item-0"><b>Utilities</b></a>
-            <a href="#" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-1">Customers</a>
-            <a href="#" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Items</a>
-            <a href="#" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Estimates</a>
+            <span class=" block px-4 py-2 text-sm bg-[#edf2f7] text-[#930027]" role="menuitem" tabindex="-1" id="menu-item-0"><b>Utilities</b></span>
+            <a href="/customers" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-1">Customers</a>
+            <a href="/items" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Items</a>
+            <a href="/estimates" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Estimates</a>
         </div>
     </div>
 </div>
