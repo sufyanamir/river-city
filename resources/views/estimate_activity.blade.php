44876960961ad6cec32516ecf259e484f7d60089
Sufyan Amir <131964521+sufyanamir@users.noreply.github.com>
2024-01-10 16:56:23 +0500

    Revert "modification in activity module."

--- resources/views/estimate_activity.blade.php
+++ resources/views/estimate_activity.blade.php
@@ -1,32 +1,27 @@
 @include('layouts.header')
-
-<div class="my-4">
-    <div class="bg-white w-full rounded-2xl shadow-lg">
-        <div class="flex justify-between p-3 bg-[#930027] text-white rounded-t-2xl">
-            <div class="text-xl font-semibold">
+<div class=" my-4">
+    <div class=" bg-white w-full rounded-2xl shadow-lg">
+        <div class=" flex justify-between p-3 bg-[#930027] text-white rounded-t-2xl">
+            <div class=" text-xl font-semibold">
                 <h4>Estimate Activity</h4>
             </div>
+            {{-- <form action="/markNotifications" method="post">
+                @csrf
+                <button id="" class="bg-white text-black  p-2 rounded-md font-medium mb-2">
+                    Mark as Read
+                </button>
+            </form> --}}
         </div>
-        
-        @if(count($activities) > 0)
-            @foreach ($activities as $activity)
-                <div class="py-1">
-                    <div class="bg-[#F5F5F5] rounded-lg p-3 m-2">
-                        <h1 class="font-semibold text-lg">{{ $activity->activity_title }}:</h1>
-                        <p>{{ $activity->activity_description }}</p>
-                    </div>
-                </div>
-                <hr>
-            @endforeach
-        @else
-            <div class="py-1 text-center">
+        @foreach ($activities as $activity)
+            <div class=" py-1">
                 <div class="bg-[#F5F5F5] rounded-lg p-3 m-2">
-                    <h1>No Activity Right Now!</h1>
+                    <h1 class=" font-semibold text-lg">{{ $activity->activity_title }}:</h1>
+                    <p>{{ $activity->activity_description }}</p>
                 </div>
             </div>
-        @endif
-
+            <hr>
+        @endforeach
     </div>
 </div>
-
+</div>
 @include('layouts.footer')
