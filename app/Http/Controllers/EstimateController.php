e5452fd03e272722c87aeaf9c638cb674141fc25
sufyanamir <sufi.canola@gmail.com>
2024-02-24 15:40:49 +0500

    changing in estimates table.

--- app/Http/Controllers/EstimateController.php
+++ app/Http/Controllers/EstimateController.php
@@ -2150,7 +2150,7 @@ class EstimateController extends Controller
                 ]);
             }
 
-            return response()->json(['success' => true, 'zr' => $zr, 'message' => 'Estimate created Successfully!', 'estimate_id' => $estimate->estimate_id], 200);
+            return response()->json(['success' => true, 'message' => 'Estimate created Successfully!', 'estimate_id' => $estimate->estimate_id], 200);
         } catch (\Exception $e) {
             return response()->json(['success' => false, 'message' => $e->getMessage()], 400);
         }
--- resources/views/components/quick-add-btn.blade.php
+++ resources/views/components/quick-add-btn.blade.php
@@ -10,7 +10,7 @@
             <span class=" block px-4 py-2 text-sm bg-[#edf2f7] text-[#930027]" role="menuitem" tabindex="-1" id="menu-item-0"><b>Utilities</b></span>
             <a href="/customers" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-1">Customers</a>
             <a href="/items" class="text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Items</a>
-            <a href="/estimates" class="addEstimate text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Estimates</a>
+            <a href="/estimates" class=" text-white block px-4 py-2 text-sm hover:bg-[#edf2f7] hover:text-[#930027] rounded-sm m-2 duration-200" role="menuitem" tabindex="-1" id="menu-item-2">Estimates</a>
         </div>
     </div>
 </div>
