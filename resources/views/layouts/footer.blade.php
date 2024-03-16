bee1109efbf259433a4a9c166e82579c13a77610
sufyanamir <sufi.canola@gmail.com>
2024-03-16 15:26:00 +0500

    changes in footer. put jquery link under the custom js file.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 </div>
 </div>
-<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/js/custom.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/js/dataTables.min.js') }}"></script>
 <script src="{{ asset('assets/js/sweetalert2.js') }}"></script>
 <script src="{{ asset('assets/js/topbar.min.js') }}"></script>
