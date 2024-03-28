82f2241faa1945ba3e908b62a232ca78e7d16a58
sufyanamir <sufi.canola@gmail.com>
2024-03-28 15:34:35 +0500

    changing in customers page.

--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -135,9 +135,11 @@ $userPrivileges = session('user_details')['user_privileges'];
                         </div>
                         <div class=" ">
                             <h5 class="text-gray-600 mb-1  font-medium text-left">City</h5>
-                            <input type="text" name="city" id="city" placeholder="City"
-                                autocomplete="given-name"
-                                class=" mb-2 w-[100%] outline-none rounded-md border-0 text-gray-400 p-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0095E5] sm:text-sm">
+                            <select name="city" id="city" autocomplete="given-name" class=" mb-2 w-[100%] outline-none rounded-md border-0 text-gray-400 p-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#0095E5] sm:text-sm">
+                                <option value="">Select City</option>
+                                <option value="wichita">Wichita</option>
+                                <option value="kansas">Kansas City</option>
+                            </select>
                         </div>
                         <div class=" ">
                             <h5 class="text-gray-600 mb-1  font-medium text-left">State/Province</h5>
@@ -234,7 +236,9 @@ $userPrivileges = session('user_details')['user_privileges'];
                     $('#company_name').val(customerDetail.customer_company_name);
                     $('#first_address').val(customerDetail.customer_primary_address);
                     $('#second_address').val(customerDetail.customer_secondary_address);
-                    $('#city').val(customerDetail.customer_city);
+
+                    $('#city').val(customerDetail.customer_city).trigger('change');
+
                     $('#state').val(customerDetail.customer_state);
                     $('#zip_code').val(customerDetail.customer_zip_code);
                     $('#tax_rate').val(customerDetail.tax_rate);
